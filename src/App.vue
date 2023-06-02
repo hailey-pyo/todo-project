f9f73ac86b01c91b6862777c75166762e1a005d6
hailey-pyo <haeyoung9627@gmail.com>
2023-06-02 10:14:48 +0900

    버그 수정 할일리스트 순서 변경 로직, css 수정

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
         @add="addTodoItem"
       ></todo-input>
       <div class="todoList_wrap">
-        <transition-group name="list" tag="ul">
+        <transition-group name="list" tag="ul" style="transition: 5s">
           <!-- <ul> -->
           <todo-list-item
             v-for="(todoItem, index) in todoItems"
@@ -20,7 +20,9 @@
             @edit="editTodoItem"
             @remove="removeTodoItem"
             @changeOrder="changeOrderTodoItem"
-            @readyYdata="readyYdata"
+            @readyYdata="resetYdata"
+            :changeStatus="changeStatus"
+            @resetChangeStatus="resetChangeStatus"
           ></todo-list-item>
           <!-- </ul> -->
         </transition-group>
@@ -33,7 +35,6 @@
 import Vue from "vue";
 import TodoInput from "./components/TodoInput.vue";
 import TodoListItem from "./components/TodoListItem.vue";
-import $ from "jquery";
 
 const STORAGE_KEY = "vue-todo-ts-v1";
 const storage = {
@@ -55,17 +56,29 @@ export interface Todo {
   ydata?: number;
 }
 
+export interface ChangeStatus {
+  status: boolean;
+  firstYdata: number;
+  lastYdata: number;
+}
+
 export default Vue.extend({
   components: { TodoInput, TodoListItem },
   data() {
     return {
       todoText: "",
       todoItems: [] as Todo[],
+      changeStatus: {
+        status: false,
+        firstYdata: 0,
+        lastYdata: 0,
+      } as ChangeStatus,
     };
   },
   methods: {
     updateTodoText(value: string) {
-      this.todoText = value;
+      let xx = value.replace(/\s{2,}/gi, " ");
+      this.todoText = xx;
     },
     addTodoItem() {
       const value = this.todoText;
@@ -75,7 +88,7 @@ export default Vue.extend({
           done: false,
           no:
             this.todoItems.length > 0
-              ? this.todoItems[this.todoItems.length - 1].no + 1
+              ? Math.max(...this.todoItems.map(v => v.no)) + 1
               : 0,
         });
         this.resetYdata();
@@ -108,115 +121,272 @@ export default Vue.extend({
       });
       storage.save(this.todoItems);
     },
+    // changeOrderTodoItem(todoItem: Todo, targetH: number, index: number) {
+    //   let targetIndex = 0;
+
+    //   console.log(todoItem.ydata + " / " + targetH);
+
+    //   if (todoItem.ydata! < targetH) {
+    //     // this.todoItems.forEach((v, i) => {
+    //     //   if (i != index && v.ydata! < targetH) {
+    //     //     targetIndex = i;
+    //     //     return;
+    //     //   }
+    //     // });
+    //     for (let i = 0; i < this.todoItems.length - 1; i++) {
+    //       // console.log(this.todoItems[i].ydata);
+    //       // console.log(this.todoItems[i + 1].ydata);
+
+    //       if (
+    //         targetH >= this.todoItems[i].ydata! &&
+    //         targetH < this.todoItems[i + 1].ydata!
+    //       ) {
+    //         targetIndex = i;
+    //         // console.log("==targetindex====" + targetIndex);
+    //       } else if (
+    //         i == this.todoItems.length - 2 &&
+    //         targetH >= this.todoItems[i + 1].ydata!
+    //       ) {
+    //         targetIndex = i + 1;
+    //         // console.log("제일끝일떼 " + targetIndex);
+    //       }
+    //     }
+    //   } else {
+    //     for (let i = 0; i < this.todoItems.length; i++) {
+    //       // console.log(this.todoItems[i].ydata);
+    //       if (targetH < this.todoItems[i].ydata!) {
+    //         targetIndex = i;
+    //         // console.log("==targetindex====" + targetIndex);
+    //         break;
+    //       }
+    //     }
+    //   }
+
+    //   console.log(index + "<- index ");
+    //   console.log(targetIndex + "<- targetIndex ");
+
+    //   let prevItems = [];
+    //   let changeItems = [];
+    //   let betweenItem = [];
+    //   let targetItem = [];
+    //   let restItem = [];
+    //   if (index != targetIndex) {
+    //     if (index < targetIndex) {
+    //       prevItems = this.todoItems.slice(0, index);
+    //       changeItems = this.todoItems.slice(index, index + 1);
+    //       betweenItem = this.todoItems.slice(index + 1, targetIndex);
+    //       targetItem = this.todoItems.slice(targetIndex, targetIndex + 1);
+    //       restItem = this.todoItems.slice(targetIndex + 1);
+
+    //       this.todoItems = [
+    //         ...prevItems,
+    //         ...betweenItem,
+    //         ...targetItem,
+    //         ...changeItems,
+    //         ...restItem,
+    //       ];
+    //     } else {
+    //       prevItems = this.todoItems.slice(0, targetIndex); // 1,2
+    //       changeItems = this.todoItems.slice(index, index + 1); // 5
+    //       betweenItem = this.todoItems.slice(targetIndex, index); // 4
+    //       // targetItem = this.todoItems.slice(targetIndex, targetIndex + 1); //
+    //       restItem = this.todoItems.slice(index + 1);
+
+    //       this.todoItems = [
+    //         ...prevItems,
+    //         ...changeItems,
+    //         ...betweenItem,
+    //         // ...targetItem,
+    //         ...restItem,
+    //       ];
+    //     }
+
+    //     this.todoItems.map((v, i) => {
+    //       v.no = i;
+    //     });
+    //     this.resetYdata();
+    //     storage.save(this.todoItems);
+    //   }
+    // },
+    // changeOrderTodoItem(todoItem: Todo, targetH: number, index: number) {
+    //   let targetIndex = 0;
+
+    //   console.log(todoItem.ydata + " / " + targetH);
+
+    //   if (todoItem.ydata! < targetH) {
+    //     // this.todoItems.forEach((v, i) => {
+    //     //   if (i != index && v.ydata! < targetH) {
+    //     //     targetIndex = i;
+    //     //     return;
+    //     //   }
+    //     // });
+    //     for (let i = 0; i < this.todoItems.length - 1; i++) {
+    //       // console.log(this.todoItems[i].ydata);
+    //       // console.log(this.todoItems[i + 1].ydata);
+
+    //       if (
+    //         targetH >= this.todoItems[i].ydata! &&
+    //         targetH < this.todoItems[i + 1].ydata!
+    //       ) {
+    //         targetIndex = i;
+    //         // console.log("==targetindex====" + targetIndex);
+    //       } else if (
+    //         i == this.todoItems.length - 2 &&
+    //         targetH >= this.todoItems[i + 1].ydata!
+    //       ) {
+    //         targetIndex = i + 1;
+    //         // console.log("제일끝일떼 " + targetIndex);
+    //       }
+    //     }
+    //   } else {
+    //     for (let i = 0; i < this.todoItems.length; i++) {
+    //       // console.log(this.todoItems[i].ydata);
+    //       if (targetH < this.todoItems[i].ydata!) {
+    //         targetIndex = i;
+    //         // console.log("==targetindex====" + targetIndex);
+    //         break;
+    //       }
+    //     }
+    //   }
+
+    //   console.log(index + "<- index ");
+    //   console.log(targetIndex + "<- targetIndex ");
+
+    //   let prevItems = [];
+    //   let changeItems = [];
+    //   let betweenItem = [];
+    //   let targetItem = [];
+    //   let restItem = [];
+    //   if (index != targetIndex) {
+    //     if (index < targetIndex) {
+    //       prevItems = this.todoItems.slice(0, index);
+    //       changeItems = this.todoItems.slice(index, index + 1);
+    //       betweenItem = this.todoItems.slice(index + 1, targetIndex);
+    //       targetItem = this.todoItems.slice(targetIndex, targetIndex + 1);
+    //       restItem = this.todoItems.slice(targetIndex + 1);
+
+    //       this.todoItems = [
+    //         ...prevItems,
+    //         ...betweenItem,
+    //         ...targetItem,
+    //         ...changeItems,
+    //         ...restItem,
+    //       ];
+    //     } else {
+    //       prevItems = this.todoItems.slice(0, targetIndex); // 1,2
+    //       changeItems = this.todoItems.slice(index, index + 1); // 5
+    //       betweenItem = this.todoItems.slice(targetIndex, index); // 4
+    //       // targetItem = this.todoItems.slice(targetIndex, targetIndex + 1); //
+    //       restItem = this.todoItems.slice(index + 1);
+
+    //       this.todoItems = [
+    //         ...prevItems,
+    //         ...changeItems,
+    //         ...betweenItem,
+    //         // ...targetItem,
+    //         ...restItem,
+    //       ];
+    //     }
+
+    //     this.todoItems.map((v, i) => {
+    //       v.no = i;
+    //     });
+    //     this.resetYdata();
+    //     storage.save(this.todoItems);
+    //   }
+    // },
     changeOrderTodoItem(todoItem: Todo, targetH: number, index: number) {
       let targetIndex = 0;
-      // if()
-      //만약 targetH의 index와 index가 같다면? -> 막아놈 -> 아래 로직 잘못됨
-      //만약 targetH보다 ydata가 크다면?
-      console.log(todoItem.ydata + " / " + targetH);
 
-      if (todoItem.ydata! < targetH) {
-        // this.todoItems.forEach((v, i) => {
-        //   if (i != index && v.ydata! < targetH) {
-        //     targetIndex = i;
-        //     return;
-        //   }
-        // });
-        for (let i = 0; i < this.todoItems.length - 1; i++) {
-          // console.log(this.todoItems[i].ydata);
-          // console.log(this.todoItems[i + 1].ydata);
+      // console.log(todoItem.ydata + " / " + targetH);
 
-          if (
-            targetH >= this.todoItems[i].ydata! &&
-            targetH < this.todoItems[i + 1].ydata!
-          ) {
-            targetIndex = i;
-            // console.log("==targetindex====" + targetIndex);
-          } else if (
-            i == this.todoItems.length - 2 &&
-            targetH >= this.todoItems[i + 1].ydata!
-          ) {
-            targetIndex = i + 1;
-            // console.log("제일끝일떼 " + targetIndex);
-          }
+      let currentIndex = 0;
+      this.todoItems.forEach((v, i) => {
+        if (todoItem.ydata === v.ydata) {
+          currentIndex = i;
+          return;
         }
-      } else {
-        for (let i = 0; i < this.todoItems.length; i++) {
-          // console.log(this.todoItems[i].ydata);
-          if (targetH < this.todoItems[i].ydata!) {
-            targetIndex = i;
-            // console.log("==targetindex====" + targetIndex);
-            break;
-          }
-        }
-      }
+      });
 
-      console.log(index + "<- index ");
-      console.log(targetIndex + "<- targetIndex ");
-
-      let prevItems = [];
-      let changeItems = [];
-      let betweenItem = [];
-      let targetItem = [];
-      let restItem = [];
-      if (index != targetIndex) {
-        if (index < targetIndex) {
-          prevItems = this.todoItems.slice(0, index);
-          changeItems = this.todoItems.slice(index, index + 1);
-          betweenItem = this.todoItems.slice(index + 1, targetIndex);
-          targetItem = this.todoItems.slice(targetIndex, targetIndex + 1);
-          restItem = this.todoItems.slice(targetIndex + 1);
-
-          this.todoItems = [
-            ...prevItems,
-            ...betweenItem,
-            ...targetItem,
-            ...changeItems,
-            ...restItem,
-          ];
-        } else {
-          prevItems = this.todoItems.slice(0, targetIndex); // 1,2
-          changeItems = this.todoItems.slice(index, index + 1); // 5
-          betweenItem = this.todoItems.slice(targetIndex, index); // 4
-          // targetItem = this.todoItems.slice(targetIndex, targetIndex + 1); //
-          restItem = this.todoItems.slice(index + 1);
-
-          this.todoItems = [
-            ...prevItems,
-            ...changeItems,
-            ...betweenItem,
-            // ...targetItem,
-            ...restItem,
-          ];
-        }
-
-        this.todoItems.map((v, i) => {
-          v.no = i;
-        });
+      const nextIndex =
+        currentIndex != this.todoItems.length - 1 ? currentIndex + 1 : 0;
+      if (nextIndex && targetH >= this.todoItems[nextIndex].ydata!) {
+        [
+          this.todoItems[currentIndex].title,
+          this.todoItems[currentIndex].no,
+          this.todoItems[currentIndex].done,
+          this.todoItems[nextIndex].title,
+          this.todoItems[nextIndex].no,
+          this.todoItems[nextIndex].done,
+        ] = [
+          this.todoItems[nextIndex].title,
+          this.todoItems[nextIndex].no,
+          this.todoItems[nextIndex].done,
+          this.todoItems[currentIndex].title,
+          this.todoItems[currentIndex].no,
+          this.todoItems[currentIndex].done,
+        ];
+        this.changeStatus.status = true;
         this.resetYdata();
         storage.save(this.todoItems);
       }
+
+      const prevIndex = currentIndex != 0 ? currentIndex - 1 : -1;
+      if (prevIndex != -1 && targetH <= this.todoItems[prevIndex].ydata!) {
+        [
+          this.todoItems[currentIndex].title,
+          this.todoItems[currentIndex].no,
+          this.todoItems[currentIndex].done,
+          this.todoItems[prevIndex].title,
+          this.todoItems[prevIndex].no,
+          this.todoItems[prevIndex].done,
+        ] = [
+          this.todoItems[prevIndex].title,
+          this.todoItems[prevIndex].no,
+          this.todoItems[prevIndex].done,
+          this.todoItems[currentIndex].title,
+          this.todoItems[currentIndex].no,
+          this.todoItems[currentIndex].done,
+        ];
+        this.changeStatus.status = true;
+        this.resetYdata();
+        storage.save(this.todoItems);
+      }
+      // this.todoItems.map((v, i) => {
+      //   v.no = i;
+      // });
     },
-    readyYdata() {
-      this.resetYdata();
+    resetChangeStatus(status: boolean) {
+      this.changeStatus.status = status;
     },
     resetYdata() {
-      const itemNodeList = document.querySelectorAll("main ul li") as NodeList;
-      itemNodeList.forEach((v, i) => {
-        setTimeout(() => {
+      setTimeout(() => {
+        // console.log("=======resetYdata 호출22222!!!!");
+        // console.log(this.todoItems);
+        const itemNodeList = document.querySelectorAll(
+          "main ul li",
+        ) as NodeList;
+        itemNodeList.forEach((v, i) => {
           let target = v as HTMLLIElement;
-          this.todoItems[i].ydata = target.offsetTop;
-          console.log(i + ": " + target.offsetTop);
-        }, 100);
-      });
+          if (this.todoItems[i]) {
+            this.todoItems[i].ydata = target.offsetTop;
+          }
+          if (i === 0) this.changeStatus.firstYdata = this.todoItems[i].ydata!;
+          if (i === this.todoItems.length - 1) {
+            this.changeStatus.lastYdata = this.todoItems[i].ydata!;
+          }
+        });
+      }, 100);
     },
   },
   created() {
     this.fetchTodoItems();
   },
   mounted() {
-    this.resetYdata();
-    storage.save(this.todoItems);
+    this.$nextTick(function () {
+      this.resetYdata();
+      storage.save(this.todoItems);
+    });
   },
 });
 </script>
--- src/components/TodoListItem.vue
+++ src/components/TodoListItem.vue
@@ -14,12 +14,14 @@
     <span v-else class="item" v-click-outside="onClickOutside">
       <div :style="itemHeight">
         <textarea
+          rows="1"
           ref="editInput"
           type="text"
-          v-model="edit.title"
+          :value="validateTitle"
           @input="editTodoTitle"
           @keyup.enter="editTodoComplete"
           :style="itemHeight"
+          autofocus
         />
         <button @click.prevent="removeTodo">
           <i class="fa-solid fa-trash-can"></i>
@@ -30,10 +32,11 @@
 </template>
 
 <script lang="ts">
-import { Todo } from "@/App.vue";
-import { MyVueRefs } from "@/types";
+import { ChangeStatus, Todo } from "@/App.vue";
+import { MyVueRefs, VueEvent } from "@/types";
 import Vue, { PropType } from "vue";
 import vClickOutside from "v-click-outside";
+import { throttle } from "lodash";
 
 Vue.use(vClickOutside);
 
@@ -57,6 +60,7 @@ export default (
       type: Object as PropType<Todo>,
     },
     index: Number,
+    changeStatus: Object as PropType<ChangeStatus>,
   },
   data() {
     return {
@@ -66,6 +70,7 @@ export default (
       },
       isDragging: false,
       order: [] as Order[],
+      originY: 0,
     };
   },
   computed: {
@@ -75,10 +80,12 @@ export default (
     itemHeight(): object {
       return { height: this.$refs.oneItem.scrollHeight + "px" };
     },
+    validateTitle(): string {
+      return this.todoItem.title.replace(/\s{2,}/gi, " ");
+    },
   },
   methods: {
-    onClickOutside(event: PointerEvent) {
-      this.editTodoTitle();
+    onClickOutside() {
       this.editTodoComplete();
     },
     toggleItem() {
@@ -89,16 +96,19 @@ export default (
       setTimeout(() => {
         const textarea = this.$refs.editInput;
         // textarea.style.height = "auto";
-        // textarea.parentElement!.style.height = textarea.scrollHeight + "px";
-        // textarea.style.height = textarea.scrollHeight + "px";
+        textarea.parentElement!.style.height = textarea.scrollHeight + "px";
+        textarea.style.height = textarea.scrollHeight + "px";
         textarea.focus();
       }, 100);
     },
-    editTodoTitle() {
+    editTodoTitle(event: VueEvent.Input<HTMLInputElement>) {
+      if (event.inputType == "insertLineBreak") {
+        return;
+      }
+      this.edit.title = event.target.value;
       const textarea = this.$refs.editInput;
       textarea.parentElement!.style.height = textarea.scrollHeight + "px";
       textarea.style.height = textarea.scrollHeight + "px";
-
       this.$emit("edit", this.todoItem, this.index, this.edit.title);
     },
     editTodoComplete() {
@@ -109,31 +119,101 @@ export default (
       this.$emit("remove", this.index);
     },
     mouseDown(e: MouseEvent) {
-      const originY = e.clientY;
+      this.originY = e.clientY;
+      console.log("============originY셋팅==========" + this.originY);
+
       const originTop = this.$refs.oneItem.offsetTop;
 
       this.isDragging = true;
       this.$emit("readyYdata");
 
-      document.onmousemove = e => {
-        if (this.isDragging) {
-          // const nextItem = this.$refs.oneItem.nextSibling as HTMLLIElement;
-          // console.log(nextItem.offsetTop);
-          this.$refs.oneItem.style.position = "absolute";
-          this.$refs.oneItem.style.boxShadow = "1px 1px 10px rgb(2,2,2)";
-          this.$refs.oneItem.style.width = "320px";
-          this.$refs.oneItem.style.top = originTop + e.clientY - originY + "px";
-        }
-      };
-      document.onmouseup = e => {
-        this.$emit("changeOrder", this.todoItem, e.clientY, this.index);
-        this.$refs.oneItem.style.boxShadow = "none";
-        this.$refs.oneItem.style.position = "relative";
-        this.$refs.oneItem.style.width = "auto";
-        this.$refs.oneItem.style.top = "auto";
+      // const throttle = (callback, delay) => {
+      //   let timer;
+      //   return event => {
+      //     // 타이머가 호출되면, 함수를 실행하고 타이머 제거
+      //     if (timer) return;
+      //     timer = setTimeout(() => {
+      //       callback(event);
+      //       timer = null;
+      //     }, delay);
+      //   };
+      // };
+      let prevOriginY = this.originY;
 
-        this.isDragging = false;
-      };
+      document.addEventListener(
+        "mousemove",
+        throttle((e: MouseEvent) => {
+          if (this.isDragging) {
+            if (this.changeStatus.status) {
+              prevOriginY = this.originY;
+              console.log("e.clientY: " + e.clientY);
+
+              this.$refs.oneItem.style.transform = "translateY(0px)";
+
+              this.originY = e.clientY;
+              console.log("순서변경 완료 originY변경! " + this.originY);
+              this.$emit("resetChangeStatus", false);
+            } else {
+              // console.log("originY: " + this.originY);
+
+              // const nextItem = this.$refs.oneItem.nextSibling as HTMLLIElement;
+              // console.log(nextItem.offsetTop);
+
+              // this.$refs.oneItem.style.position = "absolute";
+              this.$refs.oneItem.style.boxShadow = "1px 1px 10px rgb(2,2,2)";
+              // this.$refs.oneItem.style.width = "320px";
+              // this.$refs.oneItem.style.top = originTop + e.clientY - originY + "px";
+
+              //322로 돌아와야하는뎅
+              //331 296 만약 바뀌면 transform 0 & 현재 originY = e.clientY
+              const dy =
+                e.clientY <= this.changeStatus.firstYdata - 50 ||
+                e.clientY >= this.changeStatus.lastYdata + 50
+                  ? 0
+                  : e.clientY - this.originY;
+              // console.log(e.clientY <= this.changeStatus.firstYdata - 50);
+              // console.log(e.clientY >= this.changeStatus.lastYdata + 50);
+              // console.log("dy: " + dy);
+              //originTop + e.clientY - originY
+              //원래위치 + e.clientY - originY
+              console.log(prevOriginY + " / " + this.originY);
+
+              if (prevOriginY == this.originY) {
+                // this.$refs.oneItem.style.transform = "translateY(" + dy + "px)";
+              }
+              this.$refs.oneItem.style.transform =
+                "translateY(" + dy / 7 + "px)";
+              this.$emit("changeOrder", this.todoItem, e.clientY, this.index);
+            }
+          }
+        }, 100),
+      );
+      document.addEventListener(
+        "mouseup",
+        e => {
+          this.$refs.oneItem.style.boxShadow = "none";
+          // this.$refs.oneItem.style.position = "relative";
+          // this.$refs.oneItem.style.width = "auto";
+          // this.$refs.oneItem.style.top = "auto";
+          this.$refs.oneItem.style.transform = "translateY(0px)";
+
+          this.isDragging = false;
+        },
+        { once: true },
+      );
+      // document.addEventListener(
+      //   "mouseup",
+      //   e => {
+      //     this.$emit("changeOrder", this.todoItem, e.clientY, this.index);
+      //     this.$refs.oneItem.style.boxShadow = "none";
+      //     // this.$refs.oneItem.style.position = "relative";
+      //     this.$refs.oneItem.style.width = "auto";
+      //     // this.$refs.oneItem.style.top = "auto";
+
+      //     this.isDragging = false;
+      //   },
+      //   { once: true },
+      // );
     },
   },
 });
